abbf8def0a481664616ffba94609211c51e831d8
yashkumawat03 <152025776+yashkumawat03@users.noreply.github.com>
2026-02-20 17:48:00 +0530

    Time: 0 ms (100%), Space: 29.4 MB (99.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    void reverse(vector<int> & nums, int start, int end){
-        while(start<end){
+    void reverse(vector<int> &nums, int start, int end){
+        while(start<=end){
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
@@ -11,7 +11,7 @@ public:
     }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
+        k = k%n;
         reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
