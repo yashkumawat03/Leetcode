9a96be6f18f6b745cba1e8acbe888a409540bb23
yashkumawat03 <152025776+yashkumawat03@users.noreply.github.com>
2026-03-31 08:26:10 +0530

    Time: 3 ms (35.09%), Space: 9.9 MB (87.33%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -5,17 +5,14 @@ public:
         int i = s.length() - 1;
 
         while(i >= 0){
-            // skip spaces
             while(i >= 0 && s[i] == ' ') i--;
 
             if(i < 0) break;
 
             int j = i;
 
-            // find word
             while(i >= 0 && s[i] != ' ') i--;
 
-            // add word
             if(ans.size() > 0) ans += " ";
             ans += s.substr(i + 1, j - i);
         }
