a7354d7aa550687d52a0460129653f24efa72ba9
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-04-05 11:31:56 +0530

    Time: 0 ms (100%), Space: 23.7 MB (64.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -16,17 +16,50 @@ public:
         //     }
         //     if(ans.size()==2) break;
         // }
-        unordered_map<int,int> mpp;
-        int min = n/3+1;
-        for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==min) ans.push_back(nums[i]);
-            if(ans.size()==2) break;
+        // unordered_map<int,int> mpp;
+        // int min = n/3+1;
+        // for(int i=0;i<n;i++){
+        //     mpp[nums[i]]++;
+        //     if(mpp[nums[i]]==min) ans.push_back(nums[i]);
+        //     if(ans.size()==2) break;
             
-        }
-        // for(auto it : mpp){
-        //     if(it.second>n/3) ans.push_back(it.first);
         // }
+        // // for(auto it : mpp){
+        // //     if(it.second>n/3) ans.push_back(it.first);
+        // // }
+        // return ans;
+
+        int cnt1 = 0,cnt2 =0;
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && nums[i]!=ele2){
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=ele1){
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        int count1 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele1) count1++;
+        }
+        if(count1>n/3) ans.push_back(ele1);
+
+        int count2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele2) count2++;
+        }
+        if(count2>n/3) ans.push_back(ele2);
+
         return ans;
     }
 };
