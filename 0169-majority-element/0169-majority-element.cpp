0310bbe95790ab998644347b1056e55647cffc7e
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-04-05 11:22:02 +0530

    Time: 0 ms (100%), Space: 28.2 MB (63.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -12,23 +12,21 @@ public:
         //     if(cnt > n/2) return nums[i]; 
         // }
         // return 0;
-        int candidate = 0;
-        int count = 0;
-
-        for(int num : nums){
-            if(count == 0){
-                candidate = num;
+        int element = INT_MIN;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(cnt == 0){
+                element = nums[i];
+                cnt = 1;
             }
-
-            if(num == candidate)
-                count++;
-            else
-                count--;
+            else if(nums[i]==element) cnt++;
+            else cnt--;
         }
-
-        return candidate;
-        
-            
-        
+        int count = 0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==element) cnt++;
+        }
+        if(cnt>n/2) return element;
+        return -1;
     }
 };
