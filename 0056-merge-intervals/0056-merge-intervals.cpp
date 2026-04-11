4e2d9d40179091c2b33412047e25c4f4299b02b9
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-04-11 16:50:28 +0530

    Time: 7 ms (40.69%), Space: 23.7 MB (94.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,30 +4,30 @@ public:
        
         int n = intervals.size();
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]) 
-                continue;
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0] <= end)
-                    end = max(end,intervals[j][1]);
-                else 
-                    break;
-            }
-            ans.push_back({start,end});
-        }
+        //vector<vector<int>> ans;
+        // for(int i=0;i<n;i++){
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+        //     if(!ans.empty() && end<=ans.back()[1]) 
+        //         continue;
+        //     for(int j=i+1;j<n;j++){
+        //         if(intervals[j][0] <= end)
+        //             end = max(end,intervals[j][1]);
+        //         else 
+        //             break;
+        //     }
+        //     ans.push_back({start,end});
+        // }
 
      // Optimal Approach
-    //     vector<vector<int>> ans;
-    //     for(int i=0;i<n;i++){
-    //         if(ans.empty() || intervals[i][0] > ans.back()[1])
-    //             ans.push_back(intervals[i]);
-    //         else{
-    //             ans.back()[1] = max(ans.back()[1],intervals[i][1]);
-    //         }
-    //     }
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            if(ans.empty() || intervals[i][0] > ans.back()[1])
+                ans.push_back(intervals[i]);
+            else{
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
+        }
      return ans;
     }
 };
