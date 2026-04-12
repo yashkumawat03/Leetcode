f002e3c215dbb0c8ccce0c8e3c91282687ee5817
Yash Kumawat <152025776+yashkumawat03@users.noreply.github.com>
2026-04-12 11:35:29 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.97%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[i+m] = nums2[i];
+        // for(int i=0;i<n;i++){
+        //     nums1[i+m] = nums2[i];
+        // }
+        // sort(nums1.begin(),nums1.end());
+
+        // Optimal Approach TC-> O(m+n)
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
         }
-        sort(nums1.begin(),nums1.end());
+        while(j>=0){
+            nums1[k]=nums2[j];
+            k--, j--;
+        }
     }
 };
